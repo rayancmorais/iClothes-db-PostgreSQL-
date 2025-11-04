01a2f7925972c3c3c004dc12e1af511e9878f5ad
Rayan Morais <rayan_cm2021@icloud.com>
2025-11-04 20:36:40 -0300

    fixes uploadthing import

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,16 +1,16 @@
-import { createUploadthing, type FileRouter } from 'uploadthing/next';
-import { UploadThingError } from 'uploadThing/server';
-import { auth } from '@/auth';
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+import { auth } from "@/auth";
 
 const f = createUploadthing();
 
 export const ourFileRouter = {
   imageUploader: f({
-    image: { maxFileSize: '4MB' },
+    image: { maxFileSize: "4MB" },
   })
     .middleware(async () => {
       const session = await auth();
-      if (!session) throw new UploadThingError('Unauthorized');
+      if (!session) throw new UploadThingError("Unauthorized");
       return { userId: session?.user?.id };
     })
     .onUploadComplete(async ({ metadata }) => {
